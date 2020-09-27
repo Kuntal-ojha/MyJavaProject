2e915499c0e0530d6498f156d59a03df6878ff26
Kuntal Ojha <kuntalojha2021@gmail.com>
2020-09-28 00:42:37 +0530

    Class Work

---
+++ src/com/kuntal/ojha/Basic_Object.java
@@ -0,0 +1,27 @@
+package com.kuntal.ojha;
+
+public class Basic_Object {
+    public static void main(String[] args) {
+        // Student s1 = new Studentstudent();
+        Student s1; // Ref of object of class Student;
+        s1= new Student();
+        s1.disp();
+
+    }
+
+}
+class Student
+{
+    String name;
+    int rol_no;
+
+    Student()
+    {
+        name="Name is Missing";
+    }
+    void disp()
+    {
+        System.out.println(name);
+        System.out.println(rol_no);
+    }
+}
