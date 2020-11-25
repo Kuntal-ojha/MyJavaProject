a3cce8ff441457b6b191ba09109f28a98a16d27d
Kuntal Ojha <kuntalojha2021@gmail.com>
2020-11-26 01:07:28 +0530

    26-11-2020

---
+++ src/com/kuntal/ojha/Constructor1.java
@@ -0,0 +1,15 @@
+package com.kuntal.ojha;
+
+public class Constructor1 {
+    int roll;
+    public Constructor1(int roll){
+        this.roll=roll;
+    }
+    public Constructor1(Constructor1 std)
+    {
+        this.roll=roll;
+    }
+    public void display(){
+        
+    }
+}
