feaa937107236ce5808b8d3a6bde8436036b7cc8
Kuntal Ojha <kuntalojha2021@gmail.com>
2020-11-26 01:05:56 +0530

    23-11-2020-L(Final Keyword)

---
+++ src/com/kuntal/ojha/Final_Method.java
@@ -0,0 +1,21 @@
+/*
+// Question : 02
+// Write a program to execute final method in Java.
+package com.kuntal.ojha;
+
+public class Final_Method {
+    public void display() {
+        System.out.println("Final method.");
+    }
+
+    public static void main(String[] args) {
+        Final_Variable obj=new Final_Variable();
+        obj.display();
+    }
+
+    public void display() {
+        System.out.println("Final method.");
+    }
+}
+
+ */
