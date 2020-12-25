817f869c90b8281765be371caeb9fbf9cc9c1258
Kuntal Ojha <kuntalojha2021@gmail.com>
2020-12-25 22:19:02 +0530

    25-12-2020

---
+++ src/com/kuntal/ojha/C3_DT_V_And_Array6_Square_Root.java
@@ -0,0 +1,10 @@
+package com.kuntal.ojha;
+
+public class C3_DT_V_And_Array6_Square_Root {
+    public static void main(String[] args) {
+        double a=25.0,b;
+        b=Math.sqrt(a);
+        System.out.print("Square root of 25 is ");
+        System.out.println(b);
+    }
+}
---
+++ src/com/kuntal/ojha/C3_DT_V_And_Array8.java
@@ -0,0 +1,13 @@
+package com.kuntal.ojha;
+// Demonstrate lifetime of a variable.
+public class C3_DT_V_And_Array8 {
+    public static void main(String[] args) {
+        int x;
+        for (x=0;x<3;x++) {
+            int y=-1; // Y is initialize each time block element.
+            System.out.println("y is: "+ y); // This allays prints -1
+            y=100;
+            System.out.println("y is now: "+y); // This allays print 100
+        }
+    }
+}
