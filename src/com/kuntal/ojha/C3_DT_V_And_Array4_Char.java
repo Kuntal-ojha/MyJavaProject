8f496f49d6a9126a9c8ac3a8f3772e7445e08715
Kuntal Ojha <kuntalojha2021@gmail.com>
2020-12-25 18:27:50 +0530

    25-12-2020

---
+++ src/com/kuntal/ojha/C3_DT_V_And_Array4_Char.java
@@ -0,0 +1,11 @@
+package com.kuntal.ojha;
+// Char variable behave like integer
+public class C3_DT_V_And_Array4_Char {
+    public static void main(String[] args) {
+        char ch1;
+        ch1='X';
+        System.out.println("ch1 contains "+ch1);
+        ch1++; // increment ch1
+        System.out.println("ch1 is now "+ch1);
+    }
+}
