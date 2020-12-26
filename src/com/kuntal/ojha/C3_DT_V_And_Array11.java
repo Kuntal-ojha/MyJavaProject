1289723d372a46a0e0f0a87efd1d42d470eb93fd
Kuntal Ojha <kuntalojha2021@gmail.com>
2020-12-26 19:11:29 +0530

    26-12-2020

---
+++ src/com/kuntal/ojha/C3_DT_V_And_Array11.java
@@ -0,0 +1,15 @@
+package com.kuntal.ojha;
+
+public class C3_DT_V_And_Array11 {
+    public static void main(String[] args) {
+        byte b=42;
+        char c='a';
+        short s=1024;
+        int i=50000;
+        float f=5.67f;
+        double d=.1234;
+        double result =(f*b)+(i/c)-(d*s);
+        System.out.println((f*b)+(i/c)-(d*s));
+        System.out.println("\nresult="+result);
+    }
+}
---
+++ src/com/kuntal/ojha/C3_DT_V_And_Array9.java
@@ -0,0 +1,10 @@
+package com.kuntal.ojha;
+// This program will not compile
+public class C3_DT_V_And_Array9 {
+    public static void main(String[] args) {
+        int bar=1;
+        {               // creates a new scope.
+            //int bar=2;  // Compile-time error-bar already defined!
+        }
+    }
+}
