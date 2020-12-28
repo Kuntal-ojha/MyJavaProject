0e628631d172859b94f085b89cce16e7ef69d448
Kuntal Ojha <kuntalojha2021@gmail.com>
2020-12-29 01:47:24 +0530

    28-12-2020

---
+++ src/com/kuntal/ojha/C3_DT_V_And_Array22_Array_Literals.java
@@ -0,0 +1,10 @@
+package com.kuntal.ojha;
+
+public class C3_DT_V_And_Array22_Array_Literals {
+    public static void main(String[] args) {
+        System.out.println("Hello World !\n");
+        System.out.println("two\nlines.");
+        System.out.println();
+        System.out.println("\"This is in quotes\"");
+    }
+}
