6dd7533393310c9dcafc674b86eb67436b218079
Kuntal Ojha <kuntalojha2021@gmail.com>
2021-01-05 20:55:30 +0530

    05-01-2021

---
+++ src/com/kuntal/ojha/C5_Control_Statements_18_For_Loop.java
@@ -0,0 +1,12 @@
+package com.kuntal.ojha;
+// for(initialization;condition;iteration){
+// body
+// }
+// Demonstrate the for loop.
+public class C5_Control_Statements_18_For_Loop {
+    public static void main(String[] args) {
+        int n;
+        for (n=10;n>0;n--)
+            System.out.println("tick "+n);
+    }
+}
