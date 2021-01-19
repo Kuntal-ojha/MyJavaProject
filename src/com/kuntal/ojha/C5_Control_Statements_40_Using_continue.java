f1b839ee342ea4239eb5fa9d7782058113055629
Kuntal Ojha <kuntalojha2021@gmail.com>
2021-01-19 13:48:20 +0530

    19-01-2021

---
+++ src/com/kuntal/ojha/C5_Control_Statements_40_Using_continue.java
@@ -0,0 +1,12 @@
+package com.kuntal.ojha;
+// Demonstrate continue.
+public class C5_Control_Statements_40_Using_continue {
+    public static void main(String[] args) {
+        for (int i = 0; i <10 ; i++) {
+            System.out.print(i+" ");
+            if (i%2==0)
+                continue;
+            System.out.println();
+        }
+    }
+}
